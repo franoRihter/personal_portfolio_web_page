c078e93485529adbff1a2c72af0e4cfa0ae4027a
franoRihter <frano_rihter@hotmail.com>
2024-01-07 17:20:00 +0100

    v1

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<body>
+    <div class="container">
+            <div class="izbornik2"><div class="izbornik_naslov">
+                <h1>Frano Rihter</h1>
+            </div>
+        </div>
+            <div class="izbornik2"><div class="izbornik_lista">
+                <ul class="lista">
+                    <li><a href="index.html">index</a></li>
+                    <li><a href="projekti.html">projekti</a></li>
+                    <li>upiti</li>
+                    <li><a href="blog.html">blog</a></li>
+                </ul>
+            </div>
+    </div>
+    </div>
+    <div class="paragraf1">
+        <p><?php echo "ovo je tekst";?>Ovo je tekst</p>
+    </div>
+    
+</body>
+
+</html>
